d611ce8a8aab7978dc3995f00e532175404a1212
Vikramaditya <63139007+VikramadityaDev@users.noreply.github.com>
2023-01-28 23:04:27 +0530

    comment code removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:unique_identifier/unique_identifier.dart';
-
-// import 'homepage.dart';
 import 'login_page2.dart';
 
 void main() async {
@@ -17,7 +11,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,100 +18,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class LoginScreen extends StatefulWidget {
-//   @override
-//   _LoginScreenState createState() => _LoginScreenState();
-// }
-
-// class _LoginScreenState extends State<LoginScreen> {
-//   final _formKey = GlobalKey<FormState>();
-//   late String _email;
-//   late String _password;
-//   bool _isLoading = false;
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(),
-//       body: _isLoading
-//           ? const Center(child: CircularProgressIndicator())
-//           : Form(
-//               key: _formKey,
-//               child: Column(
-//                 children: <Widget>[
-//                   TextFormField(
-//                     decoration: const InputDecoration(labelText: 'Email'),
-//                     validator: (value) {
-//                       if (value!.isEmpty) {
-//                         return 'Please enter your email';
-//                       }
-//                       return null;
-//                     },
-//                     onSaved: (value) => _email = value!,
-//                   ),
-//                   TextFormField(
-//                     decoration: const InputDecoration(labelText: 'Password'),
-//                     validator: (value) {
-//                       if (value!.isEmpty) {
-//                         return 'Please enter your password';
-//                       }
-//                       return null;
-//                     },
-//                     onSaved: (value) => _password = value!,
-//                   ),
-//                   ElevatedButton(
-//                     onPressed: _submit,
-//                     child: const Text('LOGIN'),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//     );
-//   }
-
-//   Future<void> _submit() async {
-//     if (_formKey.currentState!.validate()) {
-//       _formKey.currentState!.save();
-//       setState(() {
-//         _isLoading = true;
-//       });
-//       final response = await _auth.signInWithEmailAndPassword(
-//         email: _email,
-//         password: _password
-//       );
-//       _handleServerResponse(response);
-//     }
-//   }
-
-//   Future<void> _handleServerResponse(UserCredential response) async {
-//     setState(() {
-//       _isLoading = false;
-//     });
-//     if (response.user != null) {
-//       // login success
-//       final prefs = await SharedPreferences.getInstance();
-//       final deviceId = await _getDeviceId();
-//       prefs.setString('deviceId', deviceId);
-//       // ignore: use_build_context_synchronously
-//       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
-//     } else {
-//       // login failed
-//       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-//         content: Text('Login failed'),
-//       ));
-//     }
-//   }
-
-//   Future<String> _getDeviceId() async {
-//     String deviceId;
-//     try {
-//       deviceId = (await UniqueIdentifier.serial)!;
-//     return deviceId;
-//     } catch (e) {
-//     print(e);
-//     rethrow;
-//     }
-//   }
-// }
